c7d8f20eb3db1fd2ada94613ca41b12b332f35d0
Erkin Pardayev <erkin.pardayev@gmail.com>
2020-07-07 14:31:47 +0800

    Constructor => Book

---
+++ Problems/Book/src/Main.java
@@ -0,0 +1,11 @@
+class Book {
+	public String title;
+	public int yearOfPublishing;
+	public String[] authors;
+
+	Book(String title, int yearOfPublishing, String[] authors) {
+		this.title = title;
+		this.yearOfPublishing = yearOfPublishing;
+		this.authors = authors;
+	}
+}
